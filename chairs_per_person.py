86efd425e496edacc1d4c88bf8dc9c797f61c4d3
borgeba <borgebardsen@tromso-studenter2-dhcp6680.wifi.uit.no>
2020-02-11 15:30:35 +0100

    ...

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,7 @@
-f = open("reservations.csv")
+f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
     chairs_per_person = 50 / int(number)
+    chairs_per_person = int(chairs_per_person) 
     print("{} will get {} chairs per person".format(name, chairs_per_person))
